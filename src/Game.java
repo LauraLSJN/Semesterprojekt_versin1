ae10a9d6edce1b7e80298c6742483548f280269a
LauraLSJN <lsjn@ruc.dk>
2022-12-06 14:01:32 +0100

    Changes by christinewulffeld

--- src/Game.java
+++ src/Game.java
@@ -140,6 +140,9 @@ public class Game { //Game klassen - sætter de ting ind som vi skal bruge i vor
         detection();
         dropFoodObjects();
         shoppingBaskets.forEach(shoppingBasket -> shoppingBasket.update());
+
+       // tid.forEach(tid -> tid.countdownTimer());
+
     }
 
 
