2bf4bacb440f2915b6d20339837ed9637fe681d3
Chvargas15 <caro.vargas@hotmail.dk>
2022-12-07 16:52:15 +0100

    hej

---
+++ src/MenuButton.java
@@ -0,0 +1,6 @@
+public class MenuButton {
+    public static void main(String[] args) {
+        //JButton = a button that performs an action when clicked on
+        MyFrame MyFrame = new MyFrame();
+    }
+}
